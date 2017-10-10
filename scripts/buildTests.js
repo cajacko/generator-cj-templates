9039193040c91eafe23fb008507e6eae9e94b0e6
Charlie Jackson <charlie@charliejackson.com>
2017-10-10 11:40:40 +0100

    Delete tests folder when building tests

--- scripts/buildTests.js
+++ scripts/buildTests.js
@@ -3,38 +3,40 @@ const fs = require('fs-extra');
 const ejs = require('ejs');
 const tests = require('./tests');
 
-tests.forEach(({ controlDir, prompts }) => {
-  const file = path.join(__dirname, `../__tests__/${controlDir}/index.js`);
+fs.emptyDir(path.join(__dirname, '../__tests__/')).then(() => {
+  tests.forEach(({ controlDir, prompts }) => {
+    const file = path.join(__dirname, `../__tests__/${controlDir}/index.js`);
 
-  let backDir = '../';
+    let backDir = '../';
 
-  controlDir.split('/').forEach(() => {
-    backDir += '../';
+    controlDir.split('/').forEach(() => {
+      backDir += '../';
+    });
+
+    const promptsArr = [];
+
+    Object.keys(prompts).forEach(key => {
+      const prompt = { key, value: prompts[key] };
+
+      if (typeof prompt.value === 'string') {
+        prompt.value = `'${prompt.value}'`;
+      }
+
+      promptsArr.push(prompt);
+    });
+
+    ejs.renderFile(
+      path.join(__dirname, 'testTemplate.js'),
+      {
+        controlDir,
+        backDir,
+        prompts: promptsArr
+      },
+      {},
+      function(err, str) {
+        if (err) throw err;
+        fs.outputFile(file, str);
+      }
+    );
   });
-
-  const promptsArr = [];
-
-  Object.keys(prompts).forEach(key => {
-    const prompt = { key, value: prompts[key] };
-
-    if (typeof prompt.value === 'string') {
-      prompt.value = `'${prompt.value}'`;
-    }
-
-    promptsArr.push(prompt);
-  });
-
-  ejs.renderFile(
-    path.join(__dirname, 'testTemplate.js'),
-    {
-      controlDir,
-      backDir,
-      prompts: promptsArr
-    },
-    {},
-    function(err, str) {
-      if (err) throw err;
-      fs.outputFile(file, str);
-    }
-  );
 });
--- scripts/tests.js
+++ scripts/tests.js
@@ -1,6 +1,6 @@
 module.exports = [
   {
-    controlDir: 'npm-module/default',
+    controlDir: 'npm-module/woo',
     prompts: {
       template: 'npm-module',
       projectDescription: 'Description of the project',
@@ -9,18 +9,27 @@ module.exports = [
     }
   },
   {
-    controlDir: 'npm-module/custom',
+    controlDir: 'npm-module/test',
     prompts: {
-      templateName: 'Custom Project',
-      templateSlug: 'different-slug',
-      destinationRoot: false,
       template: 'npm-module',
-      projectDescription: 'Custom description for a custom project',
-      packageJsonKeywords: 'custom 1, custom 2',
-      packageJsonGithubUrl: 'https://github.com/cajacko/custom-project-url',
-      authorName: 'Derek Dickleworthy',
-      authorEmail: 'derek@dickleworthy.com',
-      authorUrl: 'https://dickleworthy.com'
+      projectDescription: 'Description of the project',
+      packageJsonKeywords: 'keyword1, keyword2',
+      packageJsonGithubUrl: 'https://github.com/cajacko/dummy-project-url'
     }
   }
+  // {
+  //   controlDir: 'npm-module/custom',
+  //   prompts: {
+  //     templateName: 'Custom Project',
+  //     templateSlug: 'different-slug',
+  //     destinationRoot: false,
+  //     template: 'npm-module',
+  //     projectDescription: 'Custom description for a custom project',
+  //     packageJsonKeywords: 'custom 1, custom 2',
+  //     packageJsonGithubUrl: 'https://github.com/cajacko/custom-project-url',
+  //     authorName: 'Derek Dickleworthy',
+  //     authorEmail: 'derek@dickleworthy.com',
+  //     authorUrl: 'https://dickleworthy.com'
+  //   }
+  // }
 ];
